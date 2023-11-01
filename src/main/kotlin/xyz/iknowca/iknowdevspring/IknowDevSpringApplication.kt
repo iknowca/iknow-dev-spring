440297c5726d38317360e714dfca0d26a7cf0a82
iknowca <iknowca@gmail.com>
2023-11-01 13:14:11 +0900

    init: 코틀린 스프링부트 저장소 생성

---
+++ src/main/kotlin/xyz/iknowca/iknowdevspring/IknowDevSpringApplication.kt
@@ -0,0 +1,11 @@
+package xyz.iknowca.iknowdevspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IknowDevSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<IknowDevSpringApplication>(*args)
+}
---
+++ src/test/kotlin/xyz/iknowca/iknowdevspring/IknowDevSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package xyz.iknowca.iknowdevspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IknowDevSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
